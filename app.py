a84215201345ad580e74a766faf7b829ea4d0359
aihome-sg <aihome@celindotech.com>
2026-01-16 13:57:39 +0800

    Initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask in VS Code!"
+    return "Hello, Flask Flask in VS Code!"
 
 if __name__ == "__main__":
     app.run(debug=True)
