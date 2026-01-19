1fb96d9705178e811d5a29a5cd2b8b72991a4cbe
aihome-sg <aihome@celindotech.com>
2026-01-19 13:11:10 +0800

    Save work on dev branch

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, my first real estate website!"
+    return "Hello, my first real estate website development!"
 
 if __name__ == "__main__":
     app.run(debug=True)
