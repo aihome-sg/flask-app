65ff18a704656f12d248bc783ec04f1576ee5738
aihome-sg <aihome@celindotech.com>
2026-01-19 13:00:23 +0800

    Update Flask real estate website

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask Flask in VS Code!"
+    return "Hello, my first real estate website!"
 
 if __name__ == "__main__":
     app.run(debug=True)
